cb29a5f329ae66bfc40858afb3f2d792ea7f836b
danbendersky <dbendersky@umass.edu>
2024-11-09 11:57:09 -0500

    Added extra validation checks for event schema

--- models/event.js
+++ models/event.js
@@ -2,14 +2,23 @@ const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
   title: String,
-  date: Date,
-  location: String,
-  spotsAvailable: Number,
-  spotsFilled: Number,
-  tags : [Number],
+  date: {type : Date, default: Date.now },
+  location: {type: String, required: false},
+  spotsAvailable: {type: Number, min: 1},
+  spotsFilled: {type: Number, max: spotsAvailable},
+
+  tags : {type: [Number],
+    validate: {
+      validator: function (v) {
+        return v.length <= 7;  // Custom validation to ensure the array length is 7 or less
+      },
+      message: 'You can only have a maximum of 7 tags.'
+    }
+  },
+
   host: userReference,
 });
 
-const Item = mongoose.model('Item', eventSchema);
+const events = mongoose.model('Event', eventSchema);
 
-module.exports = Item;
+module.exports = events;
