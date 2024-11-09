6e07e4efc47a071ce5f96b4690a91b9e1ae5872e
danbendersky <dbendersky@umass.edu>
2024-11-09 14:57:36 -0500

    Added get event calls

--- routes/events.js
+++ routes/events.js
@@ -3,10 +3,26 @@ const Event = require('../model/event');
 const router = express.Router();
 
 //POST new event
+router.post
 
 //GET to get single event
+router.get('/:id', async (req, res) => {
+    try {
+        res.json(req.params.id);
+     } catch (err){
+        res.status(500).json({ message: err.message })
+     }
+})
 
 //GET to get all events
+router.get('/', async (req, res) => {
+    try {
+        const events = await Event.find()
+        res.json(subscribers);
+     } catch (err){
+        res.status(500).json({ message: err.message })
+     }
+})
 
 //PATCH when user adds themselves to event-only attendees affected
 
