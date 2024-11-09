2c89d5e7d5f5a31c3b4f0873c7c8e3e6f6b9e91a
Sodiumtelluride <gelfandnat@gmail.com>
2024-11-09 18:37:10 -0500

    Added Tabs and Tab Behavior
    Co-Authored-By: Alan Achilles <122038420+alanNEF@users.noreply.github.com>

--- client/src/MyEventsBody.jsx
+++ client/src/MyEventsBody.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState}from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './MyEventsBody.css'
@@ -6,11 +6,25 @@ import MyEvents from './Home.jsx'
 import Hosting from './Hosting.jsx'
 import Attending from './Attending.jsx'
 export default function MyEventsBody() {
+    const [isHostingClicked, setIsHostingClicked] = useState(true);
+
+    const toggleClicked = () => {
+        setIsHostingClicked(!isHostingClicked);
+    };
+    
     return (
         <>
+            <div id="tabs-container">
+                <div id="tabs">
+                    <div onClick={toggleClicked} id="hosting-tab" className={isHostingClicked ? 'clicked' : ''}>Hosting</div>
+                    <div onClick={toggleClicked} id="attending-tab" className={!isHostingClicked ? 'clicked' : ''}>Attending</div>
+                </div>
+                
+                {/* <Attending/>             */}
+            </div>
             <Hosting/>
-            {/* <Attending/>             */}
         </>
+        
         
         
     )
