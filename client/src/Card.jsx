c89a359966e46b81170bcab1245e0ef8aa76e761
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-11-09 00:12:38 -0500

    Card logic and rendering
    Co-Authored-By: Nathan Gelfand <69129208+Sodiumtelluride@users.noreply.github.com>

--- client/src/Card.jsx
+++ client/src/Card.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 export default function Card(props){
+    const tags = props.tags.map((tag)=>{
+        if(tag === 0){
+            return <div className="social-tag">Social</div>
+        } else if(tag === 1){
+            return <div className="outdoor-tag">Outdoor</div>
+        } else if(tag === 2){
+            return <div className="sports-tag">Sports/Fitness</div>
+        } else if(tag === 3){
+            return <div className="movies">Movies</div>
+        } else if(tag === 4){
+            return <div className="gaming">Gaming</div>
+        } else if(tag === 5){
+            return <div className="food-tag">Food</div>
+        } else if(tag === 6){
+            return <div className="art-tag">Art</div>
+        } 
+    })
     return(
         <div className="card-container">
             {props.spots-filled && <div className="spots-filled-tag">Spots Filled</div>}
-            <img src="" alt="" />
-            <h1>{props.title}</h1>
-            <p>{props.date}, {props.time}</p>
-            <p>{props.location}</p>
-            <p>{props.spots.filled}/{props.spots.available}</p>
+            <img className="card-img" src={props.img} />
+            <h1 className="card-title">{props.title}</h1>
+            <p className="card-info">{props.date}, {props.time}</p>
+            <p className="card-info">{props.location}</p>
+            <p className="card-info">{props.spots.filled}/{props.spots.available} Spots Filled</p>
+            {tags != [] && <div className="tags">{tags}</div>}
+            <div className="card-username">
+                {props.username}
+            </div>
         </div>
     )
 }
