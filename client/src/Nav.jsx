c079a8d7056e7da82138c5d6af088b8b5d5083d0
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-11-08 23:14:34 -0500

    Fixed pfp

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import "./Nav.css"
+import pfp from "./assets/pfp.svg"
 export default function Nav(){
     return(
         <nav id="nav">
@@ -9,7 +10,7 @@ export default function Nav(){
             </ul>
             <div id="account-add-button">
                 <button id="add-btn">+</button>
-                <img className="user-pfp" id="nav-pfp" src="./assets/pfp.svg"/>
+                <img className="user-pfp" id="nav-pfp" src={pfp} />
             </div>
         </nav>
     )
