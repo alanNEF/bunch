9b36967cc0809785bd13a687310d341b0f51b9d7
danbendersky <dbendersky@umass.edu>
2024-11-09 15:08:59 -0500

    Added initial, basic get requests for events

--- routes/events.js
+++ routes/events.js
@@ -3,26 +3,34 @@ const Event = require('../models/event');
 const router = express.Router();
 
 //POST new event
-router.post
+
 
 //GET to get single event
 router.get('/:id', async (req, res) => {
     try {
-        res.json(req.params.id);
-     } catch (err){
-        res.status(500).json({ message: err.message })
-     }
-})
+        const event = await Event.findById(req.params.id);
+        if (!user) {
+            res.status(404).json({ message: 'Event not found with id ' + req.params.id })
+        }
+        res.json(event);
+    } catch (err){
+        res.status(500).json({ message: "Server error: can't get all events" })
+    }
+});
 
-//GET to get all events
+//GET to get all events by most recent
 router.get('/', async (req, res) => {
     try {
         const events = await Event.find()
         res.json(subscribers);
      } catch (err){
-        res.status(500).json({ message: err.message })
+        res.status(500).json({ message: "Server error: can't get all events" })
      }
-})
+});
+
+//GET by most recent AND following
+
+//GET by reccomended (optional)
 
 //PATCH when user adds themselves to event-only attendees affected
 
