772d5bf165b5961be8baf9efb88dd77379e93c64
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-11-10 00:28:17 -0500

    Update Home.jsx
    Co-Authored-By: Nathan Gelfand <69129208+Sodiumtelluride@users.noreply.github.com>

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -13,24 +13,6 @@ function Home() {
     <>
       <Nav />
       <CardList />
-      {/* <Card
-
-        event = {{
-          img : image,
-          title : 'Hike at Mt.Sugarloaf',
-          date : 'November 25th',
-          time : '4 P.M',
-          location : 'Mt.Sugarloaf',
-          spotsAvailable : 10,
-          attendees:[{firstName:'Alan', lastName:'Achilles', userName: 'alanNEF'}],
-          tags : [0,1,2,3,4,5]
-        }}
-        user = {{
-          firstName: 'Alan',
-          lastName: 'Achilles',
-          userName: 'balch'
-        }}
-      /> */}
     </>
   )
 }
