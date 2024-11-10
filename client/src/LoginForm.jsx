962f80019bb9e042e6063b819c3654dbc7e71257
Mehul Patwari <themehulpatwari@gmail.com>
2024-11-09 22:22:25 -0500

    Connected frontend login to backend
    Co-Authored-By: Nathan Gelfand <69129208+Sodiumtelluride@users.noreply.github.com>

--- client/src/LoginForm.jsx
+++ client/src/LoginForm.jsx
@@ -2,18 +2,54 @@ import './LoginForm.css'
 import React from 'react'
 import { useState } from 'react'
 export default function LoginForm() {
+    const [formData, setFormData] = useState({
+        email: '',
+        passcode: ''
+    });
+    const handleChange=(e)=>
+    {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    }
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        const dataToSubmit = {
+            ...formData,
+        };
+        try {
+            const response = await fetch('http://localhost:3000/users/passcode', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dataToSubmit),
+            });
+            const data = await response.json();
+            if (response.status === 200) {
+                window.location.href = '/..index.html';
+            } else {
+                console.log(data);
+                alert('Either your passcode or email is wrong.');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return (
         <>
 
-            <form id="login-form">
+            <form onSubmit={handleSubmit} id="login-form">
                 <h1 id="logo">Bunch.</h1>
                 <div className="login-input">
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" required/>
+                    <input type="email" id="email" name="email" onChange={handleChange} required/>
                 </div>
                 <div className="login-input">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" required/>
+                    <label htmlFor="passcode">Password</label>
+                    <input type="password" id="passcode" name="passcode" onChange={handleChange} required/>
                 </div>
                 <button type="submit" id="login-button">Log In</button>
                
@@ -22,5 +58,4 @@ export default function LoginForm() {
         
 
     )
-    
 }
