f8d44ff247de35bec57ffe40be32d5670b452b4e
Alan Achilles <122038420+alanNEF@users.noreply.github.com>
2024-11-10 08:59:08 -0500

    Logo

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react"
 import "./Nav.css"
 import pfp from "./assets/pfp.svg"
 import AddEvent from "./AddEvent"
+import logo from "./assets/bunch_website_logo.png"
 
 export default function Nav(){
    
@@ -12,6 +13,7 @@ export default function Nav(){
                 <li  className="nav-page"><a href="../index.html">Home</a></li>
                 <li className="nav-page"><a href="../my-events.html">My Events</a></li>
             </ul>
+            <img id="nav-logo" src={logo} />
             <div id="account-add-button">
                 <AddEvent/>
                 <img className="user-pfp" id="nav-pfp" src={pfp} />
